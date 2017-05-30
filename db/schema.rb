f900e3808db8664e33a889d265097ab935ac060f
Ryunosuke Murata <ryunosuke77552@gmail.com>
2017-05-30 13:47:11 +0900

    create entry-model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170530043441) do
+ActiveRecord::Schema.define(version: 20170530044618) do
+
+  create_table "entries", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "title"
+    t.datetime "published"
+    t.text     "content",    limit: 65535
+    t.string   "url"
+    t.string   "author"
+    t.integer  "feed_id"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "feeds", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
