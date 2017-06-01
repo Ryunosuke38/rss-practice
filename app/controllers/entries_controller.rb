d99d5de4dc5de513a997e415de401d59ab27bd7c
Ryunosuke Murata <ryunosuke77552@gmail.com>
2017-06-01 21:27:14 +0900

    fix view-file

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,10 +1,6 @@
 class EntriesController < ApplicationController
   before_action :set_feed, only: :index
 
-  def index
-    @entries = Entry.all.order('published desc')
-  end
-
   def show
     @entry = Entry.find(params[:id])
   end
